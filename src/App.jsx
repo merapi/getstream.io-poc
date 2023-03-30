7de21d623f4ee29782da9f8433445fcd248e7b61
Marcin Pietruszka <marcin@webskill.pl>
2023-03-30 15:48:35 +0200

    Fix basename

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ export default function App() {
 
   return (
     <StreamApp token={user.token} appId={APP_ID} apiKey={API_KEY}>
-      <Router basename="/getstream.io-poc/">
+      <Router basename="/getstream.io-poc">
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<StartPage />} />
