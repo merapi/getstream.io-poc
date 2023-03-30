befbed83f95dff40fe583af5465fd7c436e758d5
Marcin Pietruszka <marcin@webskill.pl>
2023-03-30 14:54:26 +0200

    Init

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import { useEffect, useState } from "react";
+import { StreamClient } from "getstream";
+import { StreamApp } from "react-activity-feed";
+import users from "./users";
+import { getFromStorage } from "./utils/storage";
+import StartPage from "./pages/StartPage";
+import ScrollToTop from "./components/ScrollToTop";
+
+import HomePage from "./pages/HomePage";
+import Profile from "./pages/Profile";
+import Thread from "./pages/Thread";
+
+const APP_ID = "1241168";
+const API_KEY = "ys3pbu6x7zee";
+
+window.global = window; // Hacky fix for Vite and some libs
+
+export default function App() {
+  const userId = getFromStorage("user");
+
+  const user = users.find((u) => u.id === userId) || users[0];
+
+  const [client, setClient] = useState(null);
+
+  useEffect(() => {
+    async function init() {
+      const client = new StreamClient(API_KEY, user.token, APP_ID);
+
+      await client.user(user.id).getOrCreate({ ...user, token: "" });
+
+      setClient(client);
+    }
+
+    init();
+  }, []);
+
+  if (!client) return <></>;
+
+  return (
+    <StreamApp token={user.token} appId={APP_ID} apiKey={API_KEY}>
+      <Router>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<StartPage />} />
+          <Route element={<HomePage />} path="/home" />
+          <Route element={<Profile />} path="/:user_id" />
+          <Route element={<Thread />} path="/:user_id/status/:id" />
+        </Routes>
+      </Router>
+    </StreamApp>
+  );
+}
---
+++ src/users.js
@@ -0,0 +1,44 @@
+const users = [
+  {
+    id: "iamdillion",
+    name: "Dillion",
+    image: "https://dillionmegida.com/img/deee.jpg",
+    bio: "Just here, doing my thing. Developer advocate at @getstream_io",
+    token:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiaWFtZGlsbGlvbiJ9.-JkybY1_rvhOqmLymXz-vGQBLaxUV9vmc_J7mltyG0c",
+  },
+  {
+    id: "getstream_io",
+    name: "Stream",
+    image: "https://avatars.githubusercontent.com/u/8597527?s=200&v=4",
+    bio: "Deploy activity feeds and chat at scale with Stream – an API driven platform powering over a billion end users. Get started at http://getstream.io.",
+    token:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZ2V0c3RyZWFtX2lvIn0.zvLh5NWnKYyxec9iuWwk6k3EVpq_G4mDQSIxe4zzcag",
+  },
+  {
+    id: "jake",
+    name: "Jake",
+    image: "https://picsum.photos/seed/jake2/300/300",
+    bio: "Just Jake, nothing much",
+    token:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiamFrZSJ9.7hdDNH7tKrBEHqT0MokLKzXTsJhqKY4bVZVncK5ymhA",
+  },
+  {
+    id: "joe",
+    name: "Joe",
+    image: "https://picsum.photos/seed/joe1/200/200",
+    bio: "How are you?",
+    token:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiam9lIn0.DVmW79GjyYbCYMyn4nLgzpus-z35wT4NOPjyneMOoCM",
+  },
+  {
+    id: "mike",
+    name: "Mike",
+    image: "https://picsum.photos/seed/mike7/400/400",
+    bio: "I am mike here. I do things on #react and #javascript",
+    token:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibWlrZSJ9.s41dLOTos-_3JJuEOKYqetgYjh93jqmuXTWlHpSrVi4",
+  },
+];
+
+export default users;
